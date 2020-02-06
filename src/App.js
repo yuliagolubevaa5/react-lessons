230450895c93f502fc5f5e313bffb0f7a3bd8a84
yuliagolubevaa <yuliagolubevaa5@gmail.com>
2020-02-07 01:40:58 +0300

    react-menu

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./Components/Header";
+
+function App() {
+  return (
+      <div>
+        <Header />
+      </div>
+  );
+}
+
+export default App;
